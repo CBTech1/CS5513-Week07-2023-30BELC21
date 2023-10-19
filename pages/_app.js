0db6a29b4db9be01dbf131bbe03096c96cdb1cfc
CBTech1 <101056464+CBTech1@users.noreply.github.com>
2023-10-19 07:41:49 +0000

    I made a demonstration application with a tutorial and this is the code from the tutorial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 // note from Ethan: import '@/' not working properly in replit workspaces
 
 import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+    <Component {...pageProps} />
+    </ChakraProvider>
+    );
 }
